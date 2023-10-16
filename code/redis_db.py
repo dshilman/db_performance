1a08643e4307ea8540b149ea31593f2685e84649
David Shilman <shilmantech@gmail.com>
2023-10-15 20:30:51 -0400

    redis

--- code/redis_db.py
+++ code/redis_db.py
@@ -46,4 +46,4 @@ class RedisDB (BaseDB):
 if __name__ == "__main__":
 
     file_name = 'instrument.json'
-    RedisDB(file_name=file_name, threads=2, records=2).execute()
+    RedisDB(file_name=file_name, threads=10, records=20).execute()
