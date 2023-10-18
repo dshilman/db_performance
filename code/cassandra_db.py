6b38066016bbc3d4adb85fd29e269d986630d242
David Shilman <shilmantech@gmail.com>
2023-10-18 19:48:02 -0400

    cassandra

--- code/cassandra_db.py
+++ code/cassandra_db.py
@@ -42,11 +42,8 @@ class CassandraDB(BaseDB):
         # use this if you want to use Boto to set the session parameters.
         # boto_session = boto3.Session(region_name="us-east-1")
         # auth_provider = SigV4AuthProvider(boto_session)
-        auth_provider = SigV4AuthProvider()
-
-        # Use this instead of the above line if you want to use the Default Credentials and not bother with a session.
-        # auth_provider = SigV4AuthProvider()
-
+        auth_provider = SigV4AuthProvider(region_name="us-east-1")
+       
         cluster = Cluster(['cassandra.us-east-1.amazonaws.com'], ssl_context=ssl_context, auth_provider=auth_provider,
                         port=9142)
         self.session = cluster.connect(keyspace=keyspace_name)
