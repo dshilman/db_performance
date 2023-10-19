df4ea77c2fe564d6c41c4dab47750f1222bb4cc3
David Shilman <shilmantech@gmail.com>
2023-10-19 18:55:24 -0400

    mongo

--- code/base_db.py
+++ code/base_db.py
@@ -11,6 +11,7 @@ class DecimalEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Decimal):
             return Decimal128(str(obj))
+            
         return json.JSONEncoder.default(self, obj)
 
 
--- code/mongo_db.py
+++ code/mongo_db.py
@@ -1,6 +1,8 @@
 import time
+import json
 from pymongo import MongoClient
 from base_db import BaseDB
+
 
 class MongoDB(BaseDB):
 
@@ -12,43 +14,42 @@ class MongoDB(BaseDB):
         mongo_uri = 'your_mongodb_uri'
         database_name = 'your_database_name'
         collection_name = 'your_collection_name'
-        
 
-        client = MongoClient('mongodb://dbperformance:dbperformance@docdb-2023-10-19-21-49-56.cluster-cvhepjn9lnot.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false') 
+        client = MongoClient('mongodb://dbperformance:dbperformance@docdb-2023-10-19-21-49-56.cluster-cvhepjn9lnot.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false')
 
-        ##Specify the database to be used
+        # Specify the database to be used
         db = client.dbperformance
 
-        ##Specify the collection to be used
-        self.collection = db.sample_collection
-
+        # Specify the collection to be used
+        self.collection = db.dbperformance
 
     # Function to create records in MongoDB
+
     def create_records(self, thread_id, instrument_json):
-        
 
         for i in range(1, 11):
             key = str(thread_id * 100 + i)
             start_time = time.time()
-            data = instrument_json
+            data = {"key": key,
+                    "data": json.dumps(instrument_json)}
 
             self.collection.insert_one(data)
             end_time = time.time()
             execution_time = end_time - start_time
             self.performance_data[key] = {'Create Time': execution_time}
 
-
     # Function to read records from MongoDB
+
     def read_records(self, thread_id):
-        
+
         for key in self.performance_data.keys():
             start_time = time.time()
             data = self.collection.find_one({'key': key})
             end_time = time.time()
             execution_time = end_time - start_time
             self. performance_data[key]['Read Time'] = execution_time
-        
-      
+
+
 if __name__ == "__main__":
 
     file_name = 'instrument.json'
