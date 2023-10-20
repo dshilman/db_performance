99edd7f2e7cb1d848fdeebac0f306c657281a00a
David Shilman <shilmantech@gmail.com>
2023-10-20 14:08:55 -0400

    final refactoring

--- code/script.bash
+++ code/script.bash
@@ -1,5 +1,10 @@
 sudo yum install git
 git clone https://github.com/dshilman/db_performance.git
-sudo yum install python3
 sudo git pull
+
+sudo yum install python3
+
 sudo python3 -m dynamo_db
+sudo python3 -m cassandra_db
+sudo python3 -m redis_db
+sudo python3 -m mongo_db
