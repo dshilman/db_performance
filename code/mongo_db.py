0db164b68e8dc9360bd10ad9d520be051ea2a5c1
David Shilman <shilmantech@gmail.com>
2023-12-05 16:42:58 -0500

    performnace enhancements

--- code/mongo_db.py
+++ code/mongo_db.py
@@ -5,16 +5,16 @@ from base_db import DecimalEncoder
 
 class MongoDB(BaseDB):
 
+    client = MongoClient('mongodb://<db_user>:<db_password>@docdb-2023-10-19-21-49-56.cluster-cvhepjn9lnot.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false')
+
     def __init__(self, file_name='instrument.json', threads=10, records=20):
 
         super().__init__(file_name=file_name, threads=threads, records=records)
 
         self.json_data = DecimalEncoder().encode(self.json_data)
 
-        client = MongoClient('mongodb://<db_user>:<db_password>@docdb-2023-10-19-21-49-56.cluster-cvhepjn9lnot.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false')
-
         # Specify the database to be used
-        db = client.dbperformance
+        db = MongoDB.client.dbperformance
 
         # Specify the collection to be used
         self.collection = db.dbperformance
